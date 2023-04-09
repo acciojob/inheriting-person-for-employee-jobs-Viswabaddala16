7ff7a27d906c27ea401668f80704b498e9278730
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-04-09 22:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ function Person(name, age) {
 		console.log('Hello, my name is ${this.name},I am ${this.age} years old');
 	}
 }
-
-function Employee(name, age, jobTitle) {
-	super();
+Employee extends Person{
+	function Employee(name, age, jobTitle) {
 	this.name = name;
 	this.age = age;
 	this.jobTitle = jobTitle;
 	jobGreet(){
 		console.log('Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}')
 	}
+}
+
+
 }
 const person = new Person("Allice",25);
 const employee = new Employee("Bob",30,"Manager");
