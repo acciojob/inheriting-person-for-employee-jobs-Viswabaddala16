732929ab7b5506513e6e9f6b5d40e6672281f81b
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-04-09 22:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 // complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
-	greet(){
-		console.log('Hello, my name is ${this.name},I am ${this.age} years old');
+	function Person(name, age) {
+	  this.name = name;
+	  this.age = age;
 	}
-}
-Employee extends Person{
+	
+	Person.prototype.greet = function () {
+	  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+	};
+	
 	function Employee(name, age, jobTitle) {
-	this.name = name;
-	this.age = age;
-	this.jobTitle = jobTitle;
-	jobGreet(){
-		console.log('Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}')
+	  Person.call(this, name, age);
+	  this.jobTitle = jobTitle;
 	}
-}
+	
+	Employee.prototype = Object.create(Person.prototype);
+	Employee.prototype.constructor = Employee;
+	
+	Employee.prototype.jobGreet = function () {
+	  console.log(
+		`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+	  );
+	};
+	
+	window.Person = Person;
+	window.Employee = Employee;ˇ
 
 
-}
-const person = new Person("Allice",25);
-const employee = new Employee("Bob",30,"Manager");
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
