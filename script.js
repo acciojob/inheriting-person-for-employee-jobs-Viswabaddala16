8053df7a513f6cae056ed7e4a57d40e158e6f096
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-09-20 13:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-// complete this js code
-function Person(name, age) {
-	  this.name = name;
-	  this.age = age;
-	}
-	
-	Person.prototype.greet = function () {
-	  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-	};
-	
-	function Employee(name, age, jobTitle) {
-	  Person.call(this, name, age);
-	  this.jobTitle = jobTitle;
-	}
-	
-	Employee.prototype = Object.create(Person.prototype);
-	Employee.prototype.constructor = Employee;
-	
-	Employee.prototype.jobGreet = function () {
-	  console.log(
-		`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-	  );
-	};
-	
-	window.Person = Person;
-	window.Employee = Employee;ˇ
-	
+describe("Example Test Suite", () => {
+  beforeEach(() => {
+    cy.visit(baseUrl + "/main.html"); // Ensure baseUrl is defined correctly
+  });
 
+  it("should greet a person and an employee", () => {
+    cy.window().then((win) => {
+      const Person = win.Person;
+      const Employee = win.Employee;
+      const person = new Person("Alice", 25);
+      const employee = new Employee("Bob", 30, "Manager");
 
+      // Stub the console.log method
+      cy.stub(win.console, "log").as("consoleLog");
 
+      // Call the greet methods
+      person.greet();
+      cy.get("@consoleLog").should("be.calledWith", `Hello, my name is Alice and I am 25 years old.`);
+
+      employee.jobGreet();
+      cy.get("@consoleLog").should(
+        "be.calledWith",
+        `Hello, my name is Bob, I am 30 years old, and my job title is Manager.`
+      );
+    });
+  });
+});
