46e388da030d519c3cdbcb3dd49c2962d70f2405
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-09-20 13:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-describe("Example Test Suite", () => {
+// cypress/integration/example.spec.js
+
+// This is your base URL, replace it with your actual application URL
+const baseUrl = 'http://localhost:3000'; // Replace with your application's URL
+
+describe('Example Test Suite', () => {
   beforeEach(() => {
-    cy.visit(baseUrl + "/main.html"); // Ensure baseUrl is defined correctly
+    cy.visit(`${baseUrl}/main.html`);
   });
 
-  it("should greet a person and an employee", () => {
+  it('should greet a person and an employee', () => {
     cy.window().then((win) => {
       const Person = win.Person;
       const Employee = win.Employee;
-      const person = new Person("Alice", 25);
-      const employee = new Employee("Bob", 30, "Manager");
+      const person = new Person('Alice', 25);
+      const employee = new Employee('Bob', 30, 'Manager');
 
       // Stub the console.log method
-      cy.stub(win.console, "log").as("consoleLog");
+      cy.stub(win.console, 'log').as('consoleLog');
 
       // Call the greet methods
       person.greet();
-      cy.get("@consoleLog").should("be.calledWith", `Hello, my name is Alice and I am 25 years old.`);
+      cy.get('@consoleLog').should('be.calledWith', `Hello, my name is Alice and I am 25 years old.`);
 
       employee.jobGreet();
-      cy.get("@consoleLog").should(
-        "be.calledWith",
+      cy.get('@consoleLog').should(
+        'be.calledWith',
         `Hello, my name is Bob, I am 30 years old, and my job title is Manager.`
       );
     });
